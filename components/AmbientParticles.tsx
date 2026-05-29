6a444402a3e305d96f89d64957bd6bcc62aaccba
Robin4work <rabinmishra.immanent@gmail.com>
2026-05-29 12:27:44 +0530

    done dona done done

--- components/AmbientParticles.tsx
+++ components/AmbientParticles.tsx
@@ -1,21 +1,32 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+type Particle = {
+  left: string;
+  top: string;
+  animationDelay: string;
+  animationDuration: string;
+};
+
 export default function AmbientParticles() {
-  const particles = Array.from({ length: 20 });
+  const [particles, setParticles] = useState<Particle[]>([]);
+
+  useEffect(() => {
+    const generatedParticles = Array.from({ length: 20 }).map(() => ({
+      left: `${Math.random() * 100}%`,
+      top: `${Math.random() * 100}%`,
+      animationDelay: `${Math.random() * 8}s`,
+      animationDuration: `${8 + Math.random() * 10}s`,
+    }));
+
+    setParticles(generatedParticles);
+  }, []);
 
   return (
     <div className="fixed inset-0 z-10 pointer-events-none overflow-hidden">
-      {particles.map((_, i) => (
-        <span
-          key={i}
-          className="particle"
-          style={{
-            left: `${Math.random() * 100}%`,
-            top: `${Math.random() * 100}%`,
-            animationDelay: `${Math.random() * 8}s`,
-            animationDuration: `${8 + Math.random() * 10}s`,
-          }}
-        />
+      {particles.map((particle, i) => (
+        <span key={i} className="particle" style={particle} />
       ))}
     </div>
   );
